eb55356f03f58f7f38c23e9d15f7f865bc7ce89a
Voidymite <86123187+Voidymite@users.noreply.github.com>
2025-03-06 21:22:44 -0600

    organize

--- fadeIn.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const contentElements = document.querySelector('.content');
-
-    if (contentElements) {
-        contentElements.style.opacity = 1;
-    }
-});
